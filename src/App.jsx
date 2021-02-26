ffd3c83637a1ca0a10c67e088dfc4bf1b42b81ba
Frail Bongat <jfraildev@gmail.com>
2021-02-26 18:35:57 +0800

    Added list and conditions

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { id: 'a1', name: 'Frail', age: 23 },
+      { id: 'a2', name: 'Via', age: 21 },
+    ],
+    showPerson: false,
+  };
+
+  togglePersonHandler = () => {
+    const showPerson = this.state.showPerson;
+    this.setState({ showPerson: !showPerson });
+  };
+
+  deletePersonHandler = (index) => {
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
+  };
+
+  changeNameHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((person) => {
+      return person.id === id;
+    });
+
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'Black',
+      padding: '10px 15px',
+      color: 'yellow',
+      marginTop: '3rem',
+      borderRadius: '5px',
+      border: 'none',
+      cursor: 'pointer',
+    };
+
+    let persons = null;
+
+    if (this.state.showPerson) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={this.deletePersonHandler.bind(this, index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                change={(event) => this.changeNameHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className='App'>
+        <button onClick={this.togglePersonHandler} style={style}>
+          Toggle Person
+        </button>
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
